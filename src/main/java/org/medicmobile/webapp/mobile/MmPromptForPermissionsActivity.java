6d80e15f8d235c2a5c6a9c790227ee2a46bc8f5a
Alex Anderson <anderson@medicmobile.org>
2017-10-17 14:45:13 +0000

    Extend permissions requests

--- src/main/java/org/medicmobile/webapp/mobile/MmPromptForPermissionsActivity.java
+++ src/main/java/org/medicmobile/webapp/mobile/MmPromptForPermissionsActivity.java
@@ -5,11 +5,13 @@ import android.app.Activity;
 
 import medic.android.PromptForPermissionsActivity;
 
+import static medic.android.PromptForPermissionsActivity.OPTIONAL;
 import static medic.android.PromptForPermissionsActivity.REQUIRED;
 
 public class MmPromptForPermissionsActivity extends PromptForPermissionsActivity {
 	private static final Object[][] PERMISSIONS_REQUESTS = {
-		/* location */ { REQUIRED, R.string.txtPermissionsPrompt_location, new String[] { permission.ACCESS_COARSE_LOCATION, permission.ACCESS_FINE_LOCATION } },
+		/* location */ { REQUIRED, R.string.txtPermissionsPrompt_location,   new String[] { permission.ACCESS_COARSE_LOCATION, permission.ACCESS_FINE_LOCATION } },
+		/* storage */  { OPTIONAL, R.string.txtPermissionsPrompt_fileAccess, new String[] { permission.WRITE_EXTERNAL_STORAGE } },
 	};
 
 	@Override protected Object[][] getPermissionRequests() { return PERMISSIONS_REQUESTS; }
