0fe94f326b5baa1e3c43623016a6f1a19d7e9bdf
Alex Anderson <anderson@medicmobile.org>
2017-12-04 15:41:35 +0000

    Fix checkstyle violation

--- src/main/java/org/medicmobile/webapp/mobile/EmbeddedBrowserActivity.java
+++ src/main/java/org/medicmobile/webapp/mobile/EmbeddedBrowserActivity.java
@@ -15,7 +15,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.webkit.ConsoleMessage;
 import android.webkit.ValueCallback;
 import android.widget.Toast;
 
