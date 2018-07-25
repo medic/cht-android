8b41a6aa38da212cabd2ea835690780acbb1fa28
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2018-07-26 11:14:49 +1200

    Make checkstyle happy

--- src/main/java/org/medicmobile/webapp/mobile/JavascriptUtils.java
+++ src/main/java/org/medicmobile/webapp/mobile/JavascriptUtils.java
@@ -1,6 +1,9 @@
 package org.medicmobile.webapp.mobile;
 
 final class JavascriptUtils {
+
+  private JavascriptUtils() {}
+
 	static String safeFormat(String js, Object... args) {
 		Object[] escapedArgs = new Object[args.length];
 		for(int i=0; i<args.length; ++i) {
