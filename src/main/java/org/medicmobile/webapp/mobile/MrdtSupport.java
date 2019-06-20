f883c76cf5a5a2118df1a53d850599f38470a225
Willy Tekeu <willy.tekeu@gmail.com>
2019-06-20 15:03:53 -0400

    Replace try/catcjh with if & multilines

--- src/main/java/org/medicmobile/webapp/mobile/MrdtSupport.java
+++ src/main/java/org/medicmobile/webapp/mobile/MrdtSupport.java
@@ -38,7 +38,13 @@ class MrdtSupport {
 					byte[] data = i.getByteArrayExtra("data");
 					String base64data = Base64.encodeToString(data, Base64.NO_WRAP);
 					long timeTaken = i.getLongExtra("timeTaken", 0);
-					return safeFormat("const api = angular.element(document.body).injector().get('AndroidApi'); try { api.v1.mrdtTimeTakenResponse('\"%s\"'); } catch(ex) {} api.v1.mrdtResponse('\"%s\"')", String.valueOf(timeTaken), base64data);
+
+					String javaScript = "const api = angular.element(document.body).injector().get('AndroidApi');"
+														+ "if (api.v1.mrdtTimeTakenResponse) {"
+														+ "	api.v1.mrdtTimeTakenResponse('\"%s\"');"
+														+ "}"
+														+ "api.v1.mrdtResponse('\"%s\"');";
+					return safeFormat(javaScript, String.valueOf(timeTaken), base64data);
 				} catch(Exception /*| JSONException*/ ex) {
 					warn(ex, "Problem serialising mrdt image.");
 					return safeFormat("console.log('Problem serialising mrdt image: %s')", ex);
