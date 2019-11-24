8195bb1f14a1c01041b75e8f1c34a2dea9c6a336
Willy Tekeu <willy.tekeu@gmail.com>
2019-11-24 17:56:22 -0500

    Expose device info to webapp via bridge (#95)
    * Expose device info to webapp via bridge
    
    #64

--- src/main/java/org/medicmobile/webapp/mobile/EmbeddedBrowserActivity.java
+++ src/main/java/org/medicmobile/webapp/mobile/EmbeddedBrowserActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.app.ActivityManager;
 import android.net.Uri;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
@@ -323,6 +325,10 @@ public class EmbeddedBrowserActivity extends LockableActivity {
 		maj.setAlert(new Alert(this));
 
 		maj.setLocationManager((LocationManager) this.getSystemService(Context.LOCATION_SERVICE));
+
+		maj.setActivityManager((ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE));
+
+		maj.setConnectivityManager((ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE));
 
 		container.addJavascriptInterface(maj, "medicmobile_android");
 	}
--- src/main/java/org/medicmobile/webapp/mobile/MedicAndroidJavascript.java
+++ src/main/java/org/medicmobile/webapp/mobile/MedicAndroidJavascript.java
@@ -1,20 +1,34 @@
 package org.medicmobile.webapp.mobile;
 
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
 import android.app.DatePickerDialog;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
 import android.net.TrafficStats;
 import android.os.Process;
 import android.widget.DatePicker;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,6 +48,8 @@ public class MedicAndroidJavascript {
 	private final SmsSender smsSender;
 
 	private LocationManager locationManager;
+	private ActivityManager activityManager;
+	private ConnectivityManager connectivityManager;
 	private Alert soundAlert;
 
 	public MedicAndroidJavascript(EmbeddedBrowserActivity parent) {
@@ -49,6 +65,14 @@ public class MedicAndroidJavascript {
 
 	public void setLocationManager(LocationManager locationManager) {
 		this.locationManager = locationManager;
+	}
+
+	public void setActivityManager(ActivityManager activityManager) {
+		this.activityManager = activityManager;
+	}
+
+	public void setConnectivityManager(ConnectivityManager connectivityManager) {
+		this.connectivityManager = connectivityManager;
 	}
 
 //> JavascriptInterface METHODS
@@ -230,6 +254,93 @@ public class MedicAndroidJavascript {
 		}
 	}
 
+	@org.xwalk.core.JavascriptInterface
+	@android.webkit.JavascriptInterface
+	public String getDeviceInfo() {
+		try {
+			if (activityManager == null) {
+				return jsonError("ActivityManager not set. Cannot retrieve RAM info.");
+			}
+
+			if (connectivityManager == null) {
+				return jsonError("ConnectivityManager not set. Cannot retrieve network info.");
+			}
+
+			String versionName = parent.getPackageManager()
+					.getPackageInfo(parent.getPackageName(), 0)
+					.versionName;
+			JSONObject appObject = new JSONObject();
+			appObject.put("version", versionName);
+
+			String androidVersion = Build.VERSION.RELEASE;
+			int osApiLevel = Build.VERSION.SDK_INT;
+			String osVersion = System.getProperty("os.version") + "(" + android.os.Build.VERSION.INCREMENTAL + ")";
+			JSONObject softwareObject = new JSONObject();
+			softwareObject
+					.put("androidVersion", androidVersion)
+					.put("osApiLevel", osApiLevel)
+					.put("osVersion", osVersion);
+
+			String device = Build.DEVICE;
+			String model = Build.MODEL;
+			String manufacturer = Build.BRAND;
+			String hardware = Build.HARDWARE;
+			Map<String, String> cpuInfo = getCPUInfo();
+			JSONObject hardwareObject = new JSONObject();
+			hardwareObject
+					.put("device", device)
+					.put("model", model)
+					.put("manufacturer", manufacturer)
+					.put("hardware", hardware)
+					.put("cpuInfo", new JSONObject(cpuInfo));
+
+			File dataDirectory = Environment.getDataDirectory();
+			StatFs dataDirectoryStat = new StatFs(dataDirectory.getPath());
+			long dataDirectoryBlockSize = dataDirectoryStat.getBlockSizeLong();
+			long dataDirectoryAvailableBlocks = dataDirectoryStat.getAvailableBlocksLong();
+			long dataDirectoryTotalBlocks = dataDirectoryStat.getBlockCountLong();
+			long freeMemorySize = dataDirectoryAvailableBlocks * dataDirectoryBlockSize;
+			long totalMemorySize = dataDirectoryTotalBlocks * dataDirectoryBlockSize;
+			JSONObject storageObject = new JSONObject();
+			storageObject
+					.put("free", freeMemorySize)
+					.put("total", totalMemorySize);
+
+			MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+			activityManager.getMemoryInfo(memoryInfo);
+			long totalRAMSize = memoryInfo.totalMem;
+			long freeRAMSize = memoryInfo.availMem;
+			long thresholdRAM = memoryInfo.threshold;
+			JSONObject ramObject = new JSONObject();
+			ramObject
+					.put("free", freeRAMSize)
+					.put("total", totalRAMSize)
+					.put("threshold", thresholdRAM);
+
+			NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+			JSONObject networkObject = new JSONObject();
+			if (netInfo != null && Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+				NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+				int downSpeed = networkCapabilities.getLinkDownstreamBandwidthKbps();
+				int upSpeed = networkCapabilities.getLinkUpstreamBandwidthKbps();
+				networkObject
+						.put("downSpeed", downSpeed)
+						.put("upSpeed", upSpeed);
+			}
+
+			return new JSONObject()
+					.put("app", appObject)
+					.put("software", softwareObject)
+					.put("hardware", hardwareObject)
+					.put("storage", storageObject)
+					.put("ram", ramObject)
+					.put("network", networkObject)
+					.toString();
+		} catch(Exception ex) {
+			return jsonError("Problem fetching device info: ", ex);
+		}
+	}
+
 //> PRIVATE HELPER METHODS
 	private void datePicker(String targetElement, Calendar initialDate) {
 		// Remove single-quotes from the `targetElement` CSS selecter, as
@@ -260,6 +371,27 @@ public class MedicAndroidJavascript {
 		picker.setSpinnersShown(true);
 
 		dialog.show();
+	}
+
+	private static HashMap getCPUInfo() throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("/proc/cpuinfo"));
+		String line;
+		HashMap output = new HashMap();
+		while ((line = bufferedReader.readLine()) != null) {
+			String[] data = line.split(":");
+			if (data.length > 1) {
+				String key = data[0].trim();
+				if (key.equals("model name")) {
+					output.put(key, data[1].trim());
+				}
+			}
+		}
+		bufferedReader.close();
+
+		int cores = Runtime.getRuntime().availableProcessors();
+		output.put("cores", cores);
+
+		return output;
 	}
 
 	private void logException(Exception ex) {
