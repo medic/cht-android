dfdde0564712aa52baf1d7cec8c0fbb402c798ff
Diana Barsan <barsan@medicmobile.org>
2020-07-10 12:40:37 +0300

    Indent with tabs.

--- src/main/java/org/medicmobile/webapp/mobile/TokenLogin.java
+++ src/main/java/org/medicmobile/webapp/mobile/TokenLogin.java
@@ -7,17 +7,16 @@ import android.net.Uri;
 
 import static org.medicmobile.webapp.mobile.BuildConfig.DEBUG;
 import static org.medicmobile.webapp.mobile.MedicLog.trace;
-import static org.medicmobile.webapp.mobile.SimpleJsonClient2.redactUrl;
 
 public class TokenLogin extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent appLinkIntent = getIntent();
-        Uri appLinkData = appLinkIntent.getData();
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent appLinkIntent = getIntent();
+		Uri appLinkData = appLinkIntent.getData();
 
-        if(DEBUG) trace(this, "TOKEN LOGIN=%s", appLinkData.toString());
+		if(DEBUG) trace(this, "TOKEN LOGIN=%s", appLinkData.toString());
 
-        startActivity(new Intent(Intent.ACTION_VIEW, appLinkData, this, EmbeddedBrowserActivity.class));
-        finish();
-    }
+		startActivity(new Intent(Intent.ACTION_VIEW, appLinkData, this, EmbeddedBrowserActivity.class));
+		finish();
+	}
 }
