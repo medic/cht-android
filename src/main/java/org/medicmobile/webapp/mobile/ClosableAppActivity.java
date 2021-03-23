a8e938e86b59b428109e2f59f573375c55fdeacd
Mariano Ruiz <mrsarm@gmail.com>
2021-03-23 10:54:21 -0300

    ClosableAppActivity by default closes the app only pressing back once

--- src/main/java/org/medicmobile/webapp/mobile/ClosableAppActivity.java
+++ src/main/java/org/medicmobile/webapp/mobile/ClosableAppActivity.java
@@ -3,75 +3,46 @@ package org.medicmobile.webapp.mobile;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.Toast;
 
 /**
  * Base class for activities that when back is pressed
- * two times the app closes.
+ * it closes the app instead of move backward through
+ * the history of screens, or prevent the user to either
+ * close or go back in the activities stack.
  *
- * When the user press back for the first time, a message
- * defined in the `R.string.backToExit` string is shown
- * explaining that it has to press back one more time. The
- * message can be customized in the intent with the extended
- * data "backPressedMessage"
- * (calling `intent.putExtra("backPressedMessage", "...")`).
+ * By default when the user press back the app is exited.
  *
- * Also the opposite action can be configured: the app
+ * The opposite action can be configured: the app
  * not closing nor the activity when the user press
  * back one or more times: to do so the `Intent` used to launch
  * the activity needs to be set with the extended data "isClosable"
  * to `false` (calling `intent.putExtra("isClosable", false)`).
  * No message is shown to the user when back is pressed unless
  * the extended data "backPressedMessage" is also set with the
- * desired message.
+ * desired message string.
  */
 public abstract class ClosableAppActivity extends Activity {
 
-	private static final int WAIT_MILLIS = 2 * 1000;    // Back need to be pressed 2 times before this time
-
-	private boolean closeableApp = true;                // if true, back pressing 2 times close the app
-	private boolean backToExitPressedOnce = false;      // whether back was pressed the last WAIT_MILLIS
-	private String backPressedMessage = null;			// Customized message to show when back is pressed
+	private boolean isClosable = true;			// if true, back pressing close the app
+	private String backPressedMessage = null;	// Customized message to show when back is pressed
+												// and the app activity is not closable
 
 	@Override public void onBackPressed() {
-		if (closeableApp) {
-			if (backToExitPressedOnce) {
-				finishAffinity();
-				return;
-			}
-			backToExitPressedOnce = true;
-
-			toastMessage();
-
-			new Handler().postDelayed(new Runnable() {
-				@Override public void run() {
-					// After WAIT_MILLIS clean the back pressed history
-					backToExitPressedOnce = false;
-				}
-			}, WAIT_MILLIS);
+		if (isClosable) {
+			finishAffinity();
 		} else {
 			// Back pressed events won't close the app nor close the activity
-			toastMessage();
+			if (backPressedMessage != null) {
+				Toast.makeText(this, backPressedMessage, Toast.LENGTH_SHORT).show();
+			}
 		}
 	}
 
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent i = getIntent();
-		closeableApp = i.getBooleanExtra("isClosable", true);	// not closable (the default value is true)
-		backPressedMessage = i.getStringExtra("backPressedMessage");	// default null
-	}
-
-	private void toastMessage() {
-		if (closeableApp) {
-			if (backPressedMessage != null) {
-				Toast.makeText(this, backPressedMessage, Toast.LENGTH_SHORT).show();
-			} else {
-				Toast.makeText(this, R.string.backToExit, Toast.LENGTH_SHORT).show();
-			}
-		} else if (backPressedMessage != null) {
-			Toast.makeText(this, backPressedMessage, Toast.LENGTH_SHORT).show();
-		}
+		isClosable = i.getBooleanExtra("isClosable", true);
+		backPressedMessage = i.getStringExtra("backPressedMessage");
 	}
 }
