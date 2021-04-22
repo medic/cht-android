a1f2d7d442e803e1a703628cdda3ebea827fe634
ngaruko <bede.ngaruko@gmail.com>
2021-04-22 17:32:07 +1200

    added basic tests

---
+++ src/androidTest/java/org/medicmobile/webapp/mobile/LoginTests.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.medicmobile.webapp.mobile;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import androidx.test.espresso.web.webdriver.DriverAtoms;
+import androidx.test.espresso.web.webdriver.Locator;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import java.util.Locale;
+
+import static androidx.test.espresso.web.assertion.WebViewAssertions.webContent;
+import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
+import static androidx.test.espresso.web.matcher.DomMatchers.hasElementWithId;
+import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.containsString;
+import static org.medicmobile.webapp.mobile.BuildConfig.TEST_USERNAME;
+import static org.medicmobile.webapp.mobile.BuildConfig.TEST_PASSWORD;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LoginTests {
+
+
+	//TODO: user/pass to be encrypted
+	public static final String USERNAME = BuildConfig.TEST_USERNAME;
+	public static final String PASSWORD = BuildConfig.TEST_PASSWORD;
+	public static final String SERVER_URL = BuildConfig.SERVER_URL;
+
+	@Rule
+	public ActivityTestRule<StartupActivity> mActivityRule = new ActivityTestRule<StartupActivity>(
+			StartupActivity.class);
+	SharedPreferences.Editor preferencesEditor;
+
+	@Before
+	public void setUp() {
+		Context targetContext = getInstrumentation().getTargetContext();
+		preferencesEditor = PreferenceManager.getDefaultSharedPreferences(targetContext).edit();
+		preferencesEditor.putString("api-url", SERVER_URL);
+		preferencesEditor.commit();
+	}
+
+	@Test
+	public void checkLoginPageObjects() throws Exception {
+//		preferencesEditor.putString("api-url", SERVER_URL);
+//		preferencesEditor.commit();
+
+		// Launch activity
+		mActivityRule.launchActivity(new Intent());
+		Thread.sleep(5000);//TODO: use better ways to handle delays
+		onWebView()
+				.check(webContent(hasElementWithId("form")))
+				.withElement(findElement(Locator.ID, "locale"))
+				// Verify that the response page contains the entered text
+				.check(webMatches(getText(), containsString("English")));
+		String[] codes = {"es", "en", "fr", "sw"};
+		for (String code : codes) {
+
+			onWebView().withElement(findElement(Locator.NAME, code))
+					.check(webMatches(getText(), containsString(GetLanguage(code))));
+		}
+	}
+
+	@Test
+	public void loginAsRestrictedUser() throws Exception {
+
+		onWebView().withElement(findElement(Locator.ID, "user"))
+				.perform(clearElement())
+				.perform(DriverAtoms.webKeys(USERNAME))	//to be created first
+				.withElement(findElement(Locator.ID, "password"))
+				.perform(clearElement())
+				.perform(DriverAtoms.webKeys(PASSWORD))
+				.withElement(findElement(Locator.ID, "login"))
+				.perform(webClick());
+		Thread.sleep(20000);//TODO: use better ways, takes some time to load depending on docs and whether it is first replication
+		onWebView()
+				.check(webContent(hasElementWithId("message-list")))
+				.check(webContent(hasElementWithId("messages-tab")))
+				.check(webContent(hasElementWithId("tasks-tab")))
+				.check(webContent(hasElementWithId("contacts-tab")))
+				.check(webContent(hasElementWithId("analytics-tab")))
+				.check(webContent(hasElementWithId("reports-tab")));
+	}
+
+	private String GetLanguage(String code) {
+		Locale aLocale = new Locale(code);
+		return aLocale.getDisplayName();
+	}
+}
---
+++ src/androidTest/java/org/medicmobile/webapp/mobile/SettingsDialogActivityTest.java
@@ -0,0 +1,113 @@
+package org.medicmobile.webapp.mobile;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SettingsDialogActivityTest {
+
+	private static final String WEBAPP_URL ="Webapp URL" ;
+	private static final String SERVER_ONE = "https://medic.github.io/atp";
+	private static final String SERVER_TWO = "https://gamma-cht.dev.medicmobile.org";
+	private static final String SERVER_THREE = "https://gamma.dev.medicmobile.org";
+
+	@Rule
+	public ActivityTestRule<SettingsDialogActivity> mActivityTestRule = new ActivityTestRule<>(SettingsDialogActivity.class);
+
+	@Test
+	public void testServerSelectionScreen() {
+		onView(withText("Medic Mobile")).check(matches(isDisplayed()));
+		onView(withText("Custom")).check(matches(isDisplayed()));
+		onView(withId(R.id.lstServers)).check(matches(isDisplayed()));
+//		 onView(allOf(withId(R.id.txtUrl))).
+//				 check(matches(hasChildCount(3)));//three servers
+        // servers
+		onView( withText(SERVER_ONE)).check(matches(isDisplayed()));
+		onView( withText(SERVER_TWO)).check(matches(isDisplayed()));
+		onView( withText(SERVER_THREE)).check(matches(isDisplayed()));
+		//wrong server tests
+		onView(withText("Custom")).perform(click());
+		ViewInteraction textAppUrl = onView(withId(R.id.txtAppUrl));
+		textAppUrl.check(matches(withHint(WEBAPP_URL)));
+
+		textAppUrl.perform(replaceText("something"), closeSoftKeyboard());
+		onView(withId(R.id.btnSaveSettings)).perform(click());
+		textAppUrl.check(matches(hasErrorText("must be a valid URL")));
+		pressBack();
+
+	}
+
+	@Test
+	public void testValidServerUrl() {
+		//select valid instance
+		onView( withText(SERVER_ONE)).check(matches(isDisplayed()));
+		DataInteraction linearLayout = onData(anything())
+		.inAdapterView(allOf(withId(R.id.lstServers),
+				childAtPosition(
+						withId(android.R.id.content),
+						0)))
+		.atPosition(2);
+		linearLayout.perform(click());
+
+		ViewInteraction webView = onView(
+				allOf(withId(R.id.wbvMain),
+						withParent(allOf(withId(R.id.lytWebView),
+								withParent(withId(android.R.id.content)))),
+						isDisplayed()));
+		webView.check(matches(isDisplayed()));
+	}
+
+	private static Matcher<View> childAtPosition(
+			final Matcher<View> parentMatcher, final int position) {
+
+		return new TypeSafeMatcher<View>() {
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("Child at position " + position + " in parent ");
+				parentMatcher.describeTo(description);
+			}
+
+			@Override
+			public boolean matchesSafely(View view) {
+				ViewParent parent = view.getParent();
+				return parent instanceof ViewGroup && parentMatcher.matches(parent)
+						&& view.equals(((ViewGroup) parent).getChildAt(position));
+			}
+		};
+	}
+}
