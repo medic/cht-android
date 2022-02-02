1c6fc07812abb7f8f610122bfdc271a4ff6b8859
latin-panda <66472237+latin-panda@users.noreply.github.com>
2022-02-02 12:02:15 +0700

    Suppressing UPM_UNCALLED_PRIVATE_METHOD

--- src/main/java/org/medicmobile/webapp/mobile/SimpleJsonClient2.java
+++ src/main/java/org/medicmobile/webapp/mobile/SimpleJsonClient2.java
@@ -20,6 +20,8 @@ import org.json.JSONObject;
 import static org.medicmobile.webapp.mobile.BuildConfig.DEBUG;
 import static org.medicmobile.webapp.mobile.BuildConfig.LOG_TAG;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * <p>New and improved - SimpleJsonClient2 is SimpleJsonClient, but using <code>
  * HttpURLConnection</code> instead of <code>DefaultHttpClient</code>.
@@ -159,6 +161,7 @@ public class SimpleJsonClient2 {
 		Log.d(LOG_TAG, "SimpleJsonClient2." + methodName + "() :: " + message);
 	}
 
+	@SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
 	private static void log(Exception ex, String message, Object... extras) {
 		Log.i(LOG_TAG, String.format(message, extras), ex);
 	}
