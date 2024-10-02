bef1287f3ec95727b386444c7a49f64006e15e2b
Sugat Bajracharya <sugat@medic.org>
2024-10-02 16:23:24 +0545

    Remove unused import

--- src/main/java/org/medicmobile/webapp/mobile/OpenSettingsDialogFragment.java
+++ src/main/java/org/medicmobile/webapp/mobile/OpenSettingsDialogFragment.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
