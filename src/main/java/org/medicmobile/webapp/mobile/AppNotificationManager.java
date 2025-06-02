a184ed7c61be8931eee7a4846a9371d38353b1b0
jonathanbataire <jonathanbataire@gmail.com>
2025-06-02 10:30:08 +0300

    clean up lint

--- src/main/java/org/medicmobile/webapp/mobile/AppNotificationManager.java
+++ src/main/java/org/medicmobile/webapp/mobile/AppNotificationManager.java
@@ -17,67 +17,67 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
 public class AppNotificationManager {
-  private static final String CHANNEL_ID = "cht_task_notifications";
-  private static final String CHANNEL_NAME = "CHT Task Notifications";
-  private static final int REQUEST_NOTIFICATION_PERMISSION = 1001;
+	private static final String CHANNEL_ID = "cht_task_notifications";
+	private static final String CHANNEL_NAME = "CHT Task Notifications";
+	private static final int REQUEST_NOTIFICATION_PERMISSION = 1001;
 
-  private final Context context;
-  NotificationManager manager;
+	private final Context context;
+	NotificationManager manager;
 
-  public AppNotificationManager(Context context) {
-	this.context = context;
-	manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-	createNotificationChannel();
-  }
-
-  public boolean hasNotificationPermission() {
-	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-	  return ContextCompat.checkSelfPermission(context,
-			  Manifest.permission.POST_NOTIFICATIONS)
-			  == PackageManager.PERMISSION_GRANTED;
+	public AppNotificationManager(Context context) {
+		this.context = context;
+		manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		createNotificationChannel();
 	}
-	//versions below 13
-	return true;
-  }
 
-  public void requestNotificationPermission() {
-	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
-			context instanceof Activity activity && !hasNotificationPermission()) {
-	  ActivityCompat.requestPermissions(activity,
-			  new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_NOTIFICATION_PERMISSION);
+	public boolean hasNotificationPermission() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+			return ContextCompat.checkSelfPermission(context,
+					Manifest.permission.POST_NOTIFICATIONS)
+					== PackageManager.PERMISSION_GRANTED;
+		}
+		//versions below 13
+		return true;
 	}
-  }
 
-  void showNotification(int id, String title, String contentText) {
-	Intent intent = new Intent(context, EmbeddedBrowserActivity.class);
-	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-	PendingIntent pendingIntent = PendingIntent.getActivity(
-			context,
-			0,
-			intent,
-			PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE
-	);
-	NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-			.setSmallIcon(R.drawable.ic_notification)
-			.setContentTitle(title)
-			.setContentText(contentText)
-			.setAutoCancel(true)
-			.setContentIntent(pendingIntent)
-			.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-
-	manager.notify(id, builder.build());
-  }
-
-  private void createNotificationChannel() {
-	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-	  NotificationChannel notificationChannel = new NotificationChannel(
-			  CHANNEL_ID,
-			  CHANNEL_NAME,
-			  NotificationManager.IMPORTANCE_DEFAULT
-	  );
-	  manager.createNotificationChannel(notificationChannel);
+	public void requestNotificationPermission() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+				context instanceof Activity activity && !hasNotificationPermission()) {
+			ActivityCompat.requestPermissions(activity,
+					new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_NOTIFICATION_PERMISSION);
+		}
 	}
-  }
+
+	void showNotification(int id, String title, String contentText) {
+		Intent intent = new Intent(context, EmbeddedBrowserActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+		PendingIntent pendingIntent = PendingIntent.getActivity(
+				context,
+				0,
+				intent,
+				PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE
+		);
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+				.setSmallIcon(R.drawable.ic_notification)
+				.setContentTitle(title)
+				.setContentText(contentText)
+				.setAutoCancel(true)
+				.setContentIntent(pendingIntent)
+				.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+		manager.notify(id, builder.build());
+	}
+
+	private void createNotificationChannel() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			NotificationChannel notificationChannel = new NotificationChannel(
+					CHANNEL_ID,
+					CHANNEL_NAME,
+					NotificationManager.IMPORTANCE_DEFAULT
+			);
+			manager.createNotificationChannel(notificationChannel);
+		}
+	}
 
 }
--- src/main/java/org/medicmobile/webapp/mobile/NotificationWorker.java
+++ src/main/java/org/medicmobile/webapp/mobile/NotificationWorker.java
@@ -24,98 +24,98 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class NotificationWorker extends Worker {
-  static final String TAG = "NOTIFICATION_WORKER";
-  static final int EXECUTION_TIMEOUT = 20;
+	static final String TAG = "NOTIFICATION_WORKER";
+	static final int EXECUTION_TIMEOUT = 20;
 
-  public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-	super(context, workerParams);
-  }
+	public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+		super(context, workerParams);
+	}
 
-  @SuppressLint("SetJavaScriptEnabled")
-  @NonNull
-  @Override
-  public Result doWork() {
-	SettingsStore settings = SettingsStore.in(getApplicationContext());
-	String appUrl = settings.getAppUrl();
-	CountDownLatch latch = new CountDownLatch(1);
-	new Handler(Looper.getMainLooper()).post(() -> {
-	  WebView webView = new WebView(getApplicationContext());
-	  webView.getSettings().setJavaScriptEnabled(true);
-	  webView.addJavascriptInterface(new NotificationBridge(getApplicationContext(), latch), "CHTNotificationBridge");
-	  enableStorage(webView);
+	@SuppressLint("SetJavaScriptEnabled")
+	@NonNull
+	@Override
+	public Result doWork() {
+		SettingsStore settings = SettingsStore.in(getApplicationContext());
+		String appUrl = settings.getAppUrl();
+		CountDownLatch latch = new CountDownLatch(1);
+		new Handler(Looper.getMainLooper()).post(() -> {
+			WebView webView = new WebView(getApplicationContext());
+			webView.getSettings().setJavaScriptEnabled(true);
+			webView.addJavascriptInterface(new NotificationBridge(getApplicationContext(), latch), "CHTNotificationBridge");
+			enableStorage(webView);
 
-	  webView.setWebViewClient(new WebViewClient() {
-		@Override
-		public void onPageFinished(WebView view, String url) {
-		  String js = "(async function (){" +
-				  " const api = window.CHTCore.AndroidApi;" +
-				  " if (api && api.v1 && api.v1.taskNotifications) {" +
-				  "   const tasks = await api.v1.taskNotifications();" +
-				  "   CHTNotificationBridge.onJsResult(JSON.stringify(tasks));" +
-				  " }" +
-				  "})();";
-		  view.evaluateJavascript(js, null);
+			webView.setWebViewClient(new WebViewClient() {
+				@Override
+				public void onPageFinished(WebView view, String url) {
+					String js = "(async function (){" +
+							" const api = window.CHTCore.AndroidApi;" +
+							" if (api && api.v1 && api.v1.taskNotifications) {" +
+							"   const tasks = await api.v1.taskNotifications();" +
+							"   CHTNotificationBridge.onJsResult(JSON.stringify(tasks));" +
+							" }" +
+							"})();";
+					view.evaluateJavascript(js, null);
+				}
+			});
+			webView.loadUrl(appUrl);
+		});
+		try {
+			boolean completed = latch.await(EXECUTION_TIMEOUT, TimeUnit.SECONDS);
+			if (completed) {
+				Log.d(TAG, "notification worker ran successfully!");
+			} else {
+				Log.d(TAG, "notification worker taking too long to complete");
+				return Result.failure();
+			}
+		} catch (InterruptedException e) {
+			log(e, "error: notification worker interrupted");
+			Thread.currentThread().interrupt();
+			return Result.failure();
 		}
-	  });
-	  webView.loadUrl(appUrl);
-	});
-	try {
-	  boolean completed = latch.await(EXECUTION_TIMEOUT, TimeUnit.SECONDS);
-	  if (completed) {
-		Log.d(TAG, "notification worker ran successfully!");
-	  } else {
-		Log.d(TAG, "notification worker taking too long to complete");
-		return Result.failure();
-	  }
-	} catch (InterruptedException e) {
-	  log(e, "error: notification worker interrupted");
-	  Thread.currentThread().interrupt();
-	  return Result.failure();
+
+		return Result.success();
 	}
 
-	return Result.success();
-  }
-
-  private void enableStorage(WebView container) {
-	WebSettings settings = container.getSettings();
-	settings.setDomStorageEnabled(true);
-	settings.setDatabaseEnabled(true);
-  }
-
-  public static class NotificationBridge {
-	private final Context context;
-	private final CountDownLatch latch;
-
-	NotificationBridge(Context context, CountDownLatch latch) {
-	  this.context = context;
-	  this.latch = latch;
+	private void enableStorage(WebView container) {
+		WebSettings settings = container.getSettings();
+		settings.setDomStorageEnabled(true);
+		settings.setDatabaseEnabled(true);
 	}
 
-	@JavascriptInterface
-	public void onJsResult(String data) throws JSONException {
-	  AppNotificationManager appNotificationManager = new AppNotificationManager(context.getApplicationContext());
-	  JSONArray dataArray = parseData(data);
-	  for (int i = 0; i < dataArray.length(); i++) {
-		JSONObject task = dataArray.getJSONObject(i);
-		String contentText = task.getString("contentText");
-		String title = task.getString("title");
-		long authoredOn = task.getLong("authoredOn");
-		int notificationId = (int) (authoredOn % Integer.MAX_VALUE);
-		appNotificationManager.showNotification(notificationId + i, title, contentText);
-	  }
-	  latch.countDown();
-	}
+	public static class NotificationBridge {
+		private final Context context;
+		private final CountDownLatch latch;
 
-	private JSONArray parseData(String data) {
-	  data = data.replace("^\"|\"$", "")
-			  .replace("\\\"", "\"");
-	  try {
-		return new JSONArray(data);
-	  } catch (JSONException e) {
-		log(e, "error parsing JS data");
-		return new JSONArray();
-	  }
+		NotificationBridge(Context context, CountDownLatch latch) {
+			this.context = context;
+			this.latch = latch;
+		}
+
+		@JavascriptInterface
+		public void onJsResult(String data) throws JSONException {
+			AppNotificationManager appNotificationManager = new AppNotificationManager(context.getApplicationContext());
+			JSONArray dataArray = parseData(data);
+			for (int i = 0; i < dataArray.length(); i++) {
+				JSONObject task = dataArray.getJSONObject(i);
+				String contentText = task.getString("contentText");
+				String title = task.getString("title");
+				long authoredOn = task.getLong("authoredOn");
+				int notificationId = (int) (authoredOn % Integer.MAX_VALUE);
+				appNotificationManager.showNotification(notificationId + i, title, contentText);
+			}
+			latch.countDown();
+		}
+
+		private JSONArray parseData(String data) {
+			data = data.replace("^\"|\"$", "")
+					.replace("\\\"", "\"");
+			try {
+				return new JSONArray(data);
+			} catch (JSONException e) {
+				log(e, "error parsing JS data");
+				return new JSONArray();
+			}
+		}
 	}
-  }
 
 }
