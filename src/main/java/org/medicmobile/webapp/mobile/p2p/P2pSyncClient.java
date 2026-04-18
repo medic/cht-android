c152015e3e1d262b441f3e48dc4491d93bf69a38
paulpascal <paulalogno@medic.org>
2026-04-18 08:27:37 +0000

    p2p: add missing InputStream import in P2pSyncClient

--- src/main/java/org/medicmobile/webapp/mobile/p2p/P2pSyncClient.java
+++ src/main/java/org/medicmobile/webapp/mobile/p2p/P2pSyncClient.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
